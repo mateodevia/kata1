e8c042314268c69f08337c1f3b8ec4150149f660
Carlos Vega <carlosvega@MacBook-Pro-de-Carlos.local>
2020-03-13 19:58:55 -0500

    [ AZUL - Ciclo 4 - Iteracion 1 ]

--- StringProcesing.py
+++ StringProcesing.py
@@ -3,10 +3,6 @@ class StringProcesing:
     def longitud(self, cadena):
         if cadena == '':
             return 0
-        if cadena == '1':
-            return 1
-        if cadena == '1,2':
-            return 2
         else:
             return len(cadena.split(","))
 
