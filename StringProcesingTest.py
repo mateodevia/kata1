bd32e1bcf16248f62c2cf9aa30303548e758a16f
SebastianMujica22 <42355577+SebastianMujica22@users.noreply.github.com>
2020-03-13 19:38:06 -0500

    [ROJO - ciclo3 - iteracion1 ]

--- StringProcesingTest.py
+++ StringProcesingTest.py
@@ -9,3 +9,6 @@ class TestSecuencia(unittest.TestCase):
 
     def test_longitud_1(self):
         self.assertEquals(StringProcesing().longitud('1'),1, 'String con longitud 1')
+
+    def test_longitud_2(self):
+        self.assertEquals(StringProcesing().longitud('1,2'), 2, 'String con longitud 2')
