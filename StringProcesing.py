4ba8a928306fa1a3aa421661cc3d0f67eb84ff55
Carlos Vega <carlosvega@MacBook-Pro-de-Carlos.local>
2020-03-13 21:12:33 -0500

    [ VERDE Ciclo2 Iteracion 3 ]

--- StringProcesing.py
+++ StringProcesing.py
@@ -9,12 +9,18 @@ class StringProcesing:
     def analizador_secuencia(self, cadena):
         if(cadena==''):
             return [len(cadena), None, None]
+        elif (cadena=='2'):
+            return [len(cadena), 2, 2]
+
         cadena = cadena.split(",")
         min = 100000
+
         print(cadena)
+
         for i in cadena:
             if(int(i)<int(min)):
                 min = int(i)
-        return [len(cadena), int(min)]
+
+        return [len(cadena), int(min),None]
 
 
