8842b8c0b18ece5fe6e7641758dcff84bd621db5
SebastianMujica22 <42355577+SebastianMujica22@users.noreply.github.com>
2020-03-13 20:18:04 -0500

    [VERDE - ciclo1 - iteracion2]

--- StringProcesing.py
+++ StringProcesing.py
@@ -6,7 +6,7 @@ class StringProcesing:
         else:
             return len(cadena.split(","))
 
-    def longitud_minimo(self, cadena):
-        pass
+    def analizador_secuencia(self, cadena):
+        return [ self.longitud(cadena), None]
 
 
