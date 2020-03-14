01615dde225f4ebecf884ed3ce8f2628c5e1ad21
Carlos Vega <carlosvega@MacBook-Pro-de-Carlos.local>
2020-03-13 19:26:36 -0500

    [ ROJO - Ciclo 2 - Iteracion 1]

--- StringProcesingTest.py
+++ StringProcesingTest.py
@@ -6,3 +6,6 @@ class TestSecuencia(unittest.TestCase):
 
     def test_longitud_vacio(self):
         self.assertEquals(StringProcesing().longitud(''), 0, 'Cadena vacia')
+
+    def test_longitud_1(self):
+        self.assertEquals(StringProcesing().longitud('1'),1, 'String con longitud 1')
